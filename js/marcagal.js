19b3f73119effa38e52e7171ac3fe812a15d0581
AntonioLour <39598847+AntonioLour@users.noreply.github.com>
2018-06-13 23:19:37 -0300

    Add files via upload

---
+++ js/marcagal.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$('#item2')
+.css('background-color', 'black')
+$('#item2 a')
+.css('color', 'white')
+
+})
---
+++ js/marcahome.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$('#item1')
+.css('background-color', 'black')
+$('#item1 a')
+.css('color', 'white')
+
+})
